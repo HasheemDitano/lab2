cf441e5da84f5d752fcb6f97f7856a317a5ecb6b
aoditano <aoditano@student.apc.edu.ph>
2024-02-07 16:48:36 +0800

    update

--- week8/form.php
+++ week8/form.php
@@ -99,10 +99,10 @@ echo $comment;
 echo "<br>";
 echo $gender;
 
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "myDB";
+$servername = "localhost";
+$username = "webprogmi222_sf221";
+$password = "xE*Y2nleNVvZm[!!";
+$dbname = "webprogmi222_sf221";
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
